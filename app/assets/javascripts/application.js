108a47ec5ab89d5b5dfb05d1852b6dd4a18eb8eb
Charles RT <13832188+charlesrt@users.noreply.github.com>
2024-07-02 21:52:50 +0100

    Design iterations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,3 +79,23 @@ window.GOVUKPrototypeKit.documentReady(() => {
     });
 
 })
+
+// Nested checkboxes
+var checkboxes = document.querySelectorAll('input.subOption'),
+    checkall = document.getElementById('option');
+
+for(var i=0; i<checkboxes.length; i++) {
+  checkboxes[i].onclick = function() {
+    var checkedCount = document.querySelectorAll('input.subOption:checked').length;
+
+    checkall.checked = checkedCount > 0;
+    checkall.indeterminate = checkedCount > 0 && checkedCount < checkboxes.length;
+  }
+}
+
+checkall.onclick = function() {
+  for(var i=0; i<checkboxes.length; i++) {
+    checkboxes[i].checked = this.checked;
+  }
+}
+
